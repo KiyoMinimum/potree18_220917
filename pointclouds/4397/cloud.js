51c7c0f2b07197082fe342722d7daa1936a5c405
KiyoMinimum <kwwp.23@gmail.com>
2022-09-17 08:13:31 +0900

    first commit

---
+++ pointclouds/4397/cloud.js
@@ -0,0 +1,30 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "+proj=tmerc +k_0=0.9999 +lon_0=134.333333333333 +lat_0=36 +x_0=0 +y_0=0 +ellps=GRS80 +units=m",
+    "points": 129150367,
+    "boundingBox": {
+        "lx": 89149.894757,
+        "ly": -121247.566055,
+        "lz": 172.194930,
+        "ux": 89658.854392,
+        "uy": -120738.606420,
+        "uz": 681.154565
+    },
+    "tightBoundingBox": {
+        "lx": 89181.239487,
+        "ly": -121206.522078,
+        "lz": 185.502930,
+        "ux": 89394.704956,
+        "uy": -121031.358203,
+        "uz": 259.621244
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED",
+        "CLASSIFICATION"
+    ],
+    "spacing": 3.976247,
+    "scale": 0.001000,
+    "hierarchyStepSize": 5
+}
